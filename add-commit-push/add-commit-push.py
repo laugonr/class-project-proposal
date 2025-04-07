1e334a8ba1439e6f9c69018c9cb85526ed5dad0b
Laugonr <yourruben2@gmail.com>
2025-04-06 23:51:20 -0500

    "Update files."

--- add-commit-push/add-commit-push.py
+++ add-commit-push/add-commit-push.py
@@ -6,17 +6,36 @@ print("")
 
 # Run the git status command
 resultGitStatus = subprocess.run(["git", "status"], capture_output=True, text=True)
-
 print(resultGitStatus.stdout)
-#print("STDERR:")
-#print(result.stderr)
 
+# Excuting git add
 print("Executing \"git add -A/"":")
 print("")
 
-# Run the git status command
+# Run the git add command
 resultGitAdd = subprocess.run(["git", "add", "-A"], capture_output=True, text=True)
-
 print(resultGitAdd.stdout)
 print("STDERR:")
 print(resultGitAdd.stderr)
+
+# Excuting git commit
+print("Executing \"git commit -m/"":")
+print("")
+
+# Run the git commit command
+resultGitCommit = subprocess.run(["git", "commit", "-m", "\"Update files.\""], capture_output=True, text=True)
+print(resultGitCommit.stdout)
+print("STDERR:")
+print(resultGit.stderr)
+
+# Excuting git push
+print("Executing \"git push/"":")
+print("")
+
+# Run the git push command
+resultGitPush = subprocess.run(["git", "push"], capture_output=True, text=True)
+print(resultGitPush.stdout)
+print("STDERR:")
+print(resultGitPush.stderr)
+#print("STDERR:")
+#print(result.stderr)
